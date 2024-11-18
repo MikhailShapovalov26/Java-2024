41852c8e6a06f3e80bf9c4898c59e362d3ac45db
mshapovalov <Mikhail.Shapovalov@nexign.com>
2024-11-18 21:41:11 +0300

    [18.11.2024] Первый урок

---
+++ 1.1.First_app/two.java
@@ -0,0 +1,10 @@
+
+public class two {
+    static int income = 44000;
+    static  int spending= 30000;
+    public static void main(String[] args) {
+        String difference = Integer.toString(income - spending);
+        System.out.printf("Итого: %s", difference);
+
+    }
+}
